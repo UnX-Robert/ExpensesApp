bbfeb7755c5f3bb8351c4d1ccab6d60ff418f380
Robert <antonrobert73@gmail.com>
2022-01-25 20:21:44 +0200

    Add Expenses components and Card component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import React from "react";
+import Expenses from "./components/Expenses/Expenses";
+import expenses from "./expenses.js";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
