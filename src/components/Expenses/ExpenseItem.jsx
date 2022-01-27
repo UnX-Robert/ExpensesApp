b1e7c8dbff9547fe091813a11f8ba6a59fae1860
Robert <antonrobert73@gmail.com>
2022-01-27 21:08:57 +0200

    Add See expense and hide buttons

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -4,21 +4,14 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
-  const [title, setTitle] = useState(props.title);
-
-  function clickHandler() {
-    setTitle("Clicked");
-  }
-
   return (
     <li>
       <Card className="expense-item">
         <ExpenseDate date={props.date} />
         <div className="expense-item__description">
-          <h2>{title}</h2>
+          <h2>{props.title}</h2>
           <div className="expense-item__price">{props.price}</div>
         </div>
-        <button onClick={clickHandler}>Change Title</button>
       </Card>
     </li>
   );
--- src/components/Expenses/ExpensesList.jsx
+++ src/components/Expenses/ExpensesList.jsx
@@ -1,22 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 function ExpensesList(props) {
+  const [expanded, setExpanded] = useState(false);
+
+  function handleExpanded() {
+    setExpanded((prevValue) => !prevValue);
+  }
+
   if (props.filteredExpenses.length === 0) {
     return <h2 className="headingCenter">No expenses found.</h2>;
   } else {
     return (
-      <ul className="expenses-list">
-        {props.filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            date={expense.date}
-            title={expense.title}
-            price={expense.amount}
-          />
-        ))}
-      </ul>
+      <div>
+        {expanded && (
+          <div>
+            <ul className="expenses-list">
+              {props.filteredExpenses.map((expense) => (
+                <ExpenseItem
+                  key={expense.id}
+                  date={expense.date}
+                  title={expense.title}
+                  price={expense.amount}
+                />
+              ))}
+            </ul>
+            <div className="center">
+              <button type="button" onClick={handleExpanded}>
+                Hide
+              </button>
+            </div>
+          </div>
+        )}
+
+        {!expanded && (
+          <div className="center">
+            <button type="button" onClick={handleExpanded}>
+              See expenses
+            </button>
+          </div>
+        )}
+      </div>
     );
   }
 }
